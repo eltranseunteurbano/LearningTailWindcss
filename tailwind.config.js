d352b940f33fed661c27b20feafd1d55d1b97078
Jaime Burbano <burbanojaime98@gmail.com>
2020-03-14 22:28:53 -0500

    22. Cómo integrar una card

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
       colors:{
         'primary': '#f3c614',
         'secundary': '#353535'
+      },
+      height:{
+        '80': '20rem',
+        '100': '25rem',
       }
     },
   },
